bf5205d42b9a08d71498bd94bec5de1b41d22305
Mat Gilbert <matgilbert@gmail.com>
2022-04-19 15:51:01 -0400

    added contact header

--- pages/contact.js
+++ pages/contact.js
@@ -4,13 +4,14 @@ export default function Contact() {
   return (
 
     <Layout title="normangershman.com - contact">
-<p>The Norman Gershman Photographic Archive was established in the summer of 2021 and is housed in Montpelier, Vermont; The Archive contains all of the works of Norman Gershman. While still in its formative stages as the Archive is organized, printing of newly rediscovered work is already underway.</p>
-<p>PRE-ORDERS will soon be available and each month&apos;s more prints will be added to the available list as they are properly evaluated and prepared.</p>
-<p>Should you be interested in any particular image you see in the Archives, please do let us know. We look forward to hearing from you.</p>
+    <h1>Contact</h1>
+    <p>The Norman Gershman Photographic Archive was established in the summer of 2021 and is housed in Montpelier, Vermont; The Archive contains all of the works of Norman Gershman. While still in its formative stages as the Archive is organized, printing of newly rediscovered work is already underway.</p>
+    <p>PRE-ORDERS will soon be available and each month&apos;s more prints will be added to the available list as they are properly evaluated and prepared.</p>
+    <p>Should you be interested in any particular image you see in the Archives, please do let us know. We look forward to hearing from you.</p>
 
-<p>Eric Gershman, Trustee and Director eric@normangershman.com</p>
+    <p>Eric Gershman, Trustee and Director eric@normangershman.com</p>
 
-<p>Martine Bisagni, Archivist and Curator mia@normangershman.com</p>
+    <p>Martine Bisagni, Archivist and Curator mia@normangershman.com</p>
     </Layout>
   )
 }
