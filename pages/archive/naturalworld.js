9bdeadc0bbc9351f5e13d23f1b3011d37b06d858
Mat Gilbert <matgilbert@gmail.com>
2022-05-27 10:06:12 -0400

    Added new lightbox to natural world

--- pages/archive/naturalworld.js
+++ pages/archive/naturalworld.js
@@ -1,43 +1,72 @@
 import Link from 'next/link';
+import Image from 'next/image'
 import Layout from '../../src/components/Layout';
+
 import { PhotoGallery } from '../../src/components/PhotoGallery';
 
-export default function Naturalworld() {
+import 'photoswipe/dist/photoswipe.css'
+import { Gallery, Item } from 'react-photoswipe-gallery'
 
-  var photos = [
-    {
-      src: "/img/naturalworld/nature1b.jpg",
-      width: 800,
-      height: 600
-    },
-    {
-      src: "/img/naturalworld/nature2b.jpg",
-      width: 800,
-      height: 600
-    },
-    {
-      src: "/img/naturalworld/nature3b.jpg",
-      width: 800,
-      height: 600
-    },
-    {
-      src: "/img/naturalworld/nature4b.jpg",
-      width: 800,
-      height: 600
-    },
-    {
-      src: "/img/naturalworld/nature5b.jpg",
-      width: 800,
-      height: 600
-    }
-  ]
+
+
+export default function Naturalworld() {
 
   return (
 
     <Layout title="normangershman.com - archives - natural world">
       <h2>NATURAL WORLD</h2>
       <div>
-        <PhotoGallery photos={photos} />
+
+        <Gallery>
+          <Item
+            original="/img/naturalworld/nature1b.jpg"
+            thumbnail="/img/naturalworld/nature1b.jpg"
+          >
+            {({ ref, open }) => (
+              <img ref={ref} onClick={open} src="/img/naturalworld/nature1b.jpg" width={300}  />
+            )}
+          </Item>
+
+          <Item
+            original="/img/naturalworld/nature2b.jpg"
+            thumbnail="/img/naturalworld/nature2b.jpg"
+          >
+            {({ ref, open }) => (
+              <img ref={ref} onClick={open} src="/img/naturalworld/nature2b.jpg" width={300}  />
+            )}
+          </Item>
+
+          <Item
+            original="/img/naturalworld/nature3b.jpg"
+            thumbnail="/img/naturalworld/nature3b.jpg"
+          >
+            {({ ref, open }) => (
+              <img ref={ref} onClick={open} src="/img/naturalworld/nature3b.jpg" width={300}  />
+            )}
+          </Item>
+
+          <Item
+            original="/img/naturalworld/nature4b.jpg"
+            thumbnail="/img/naturalworld/nature4b.jpg"
+          >
+            {({ ref, open }) => (
+              <img ref={ref} onClick={open} src="/img/naturalworld/nature4b.jpg" width={300}  />
+            )}
+          </Item>
+
+          <Item
+            original="/img/naturalworld/nature5b.jpg"
+            thumbnail="/img/naturalworld/nature5b.jpg"
+            width="1024"
+            height="768"
+          >
+            {({ ref, open }) => (
+              <img ref={ref} onClick={open} src="/img/naturalworld/nature5b.jpg" width={300}  />
+            )}
+          </Item>
+
+        </Gallery>
+
      </div>
     </Layout>
   )
