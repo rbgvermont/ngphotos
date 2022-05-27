4bd05d69447a2988e360cc6530fb0bfeb24516c9
Mat Gilbert <matgilbert@gmail.com>
2022-05-27 16:04:48 -0400

    lightbox for natural world is ok

--- pages/archive/naturalworld.js
+++ pages/archive/naturalworld.js
@@ -1,13 +1,16 @@
 import Link from 'next/link';
 import Image from 'next/image'
 import Layout from '../../src/components/Layout';
+import styled from 'styled-components';
 
 import { PhotoGallery } from '../../src/components/PhotoGallery';
 
 import 'photoswipe/dist/photoswipe.css'
 import { Gallery, Item } from 'react-photoswipe-gallery'
 
+const GalleryWrapper = styled.div`
 
+`;
 
 export default function Naturalworld() {
 
@@ -21,69 +24,58 @@ export default function Naturalworld() {
           <Item
             original="/img/naturalworld/nature1b.jpg"
             thumbnail="/img/naturalworld/nature1b.jpg"
-            width="1024"
-            height="768"
+            width="1580"
+            height="1058"
           >
             {({ ref, open }) => (
-              <img ref={ref} onClick={open} src="/img/naturalworld/nature1b.jpg" width={300}  />
+              <img ref={ref} onClick={open} src="/img/naturalworld/nature1b.jpg" width={400}  />
             )}
           </Item>
 
           <Item
             original="/img/naturalworld/nature2b.jpg"
             thumbnail="/img/naturalworld/nature2b.jpg"
-            width="1024"
-            height="768"
+            width="1511"
+            height="1178"
           >
             {({ ref, open }) => (
-              <img ref={ref} onClick={open} src="/img/naturalworld/nature2b.jpg" width={300}  />
+              <img ref={ref} onClick={open} src="/img/naturalworld/nature2b.jpg" width={400}  />
             )}
           </Item>
 
           <Item
             original="/img/naturalworld/nature3b.jpg"
             thumbnail="/img/naturalworld/nature3b.jpg"
-            width="1024"
-            height="768"
+            width="1031"
+            height="1037"
           >
             {({ ref, open }) => (
-              <img ref={ref} onClick={open} src="/img/naturalworld/nature3b.jpg" width={300}  />
+              <img ref={ref} onClick={open} src="/img/naturalworld/nature3b.jpg" width={400}  />
             )}
           </Item>
 
           <Item
             original="/img/naturalworld/nature4b.jpg"
             thumbnail="/img/naturalworld/nature4b.jpg"
-            width="1024"
-            height="768"
+            width="1356"
+            height="1429"
           >
             {({ ref, open }) => (
-              <img ref={ref} onClick={open} src="/img/naturalworld/nature4b.jpg" width={300}  />
+              <img ref={ref} onClick={open} src="/img/naturalworld/nature4b.jpg" width={400}  />
             )}
           </Item>
 
           <Item
             original="/img/naturalworld/nature5b.jpg"
             thumbnail="/img/naturalworld/nature5b.jpg"
-            width="1024"
-            height="768"
+            width="1946"
+            height="1473"
           >
             {({ ref, open }) => (
-              <img ref={ref} onClick={open} src="/img/naturalworld/nature5b.jpg" width={300}  />
+              <img ref={ref} onClick={open} src="/img/naturalworld/nature5b.jpg" width={400}  />
             )}
           </Item>
 
-
-          <Item
-            original="/img/naturalworld/nature5b.jpg"
-            thumbnail="/img/naturalworld/nature5b.jpg"
-            width="1024"
-            height="768"
-          >
-            {({ ref, open }) => (
-              <img ref={ref} onClick={open} src="/img/naturalworld/nature5b.jpg" width={300}  />
-            )}
-          </Item>
         </Gallery>
 
      </div>
