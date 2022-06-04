9682b81790a718646ee137dd094311ba9e6435b0
Mat Gilbert <matgilbert@gmail.com>
2022-06-04 12:23:27 -0400

    added second contact form from the docs

--- pages/contact-submitted.js
+++ pages/contact-submitted.js
@@ -28,6 +28,22 @@ export default function Contact() {
     <div>
       <p>Thank you for contacting us we will get back to you soon</p>
     </div>
+
+    <form name="contact-submitted" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
+
     </FormWrapper>
     </Layout>
   )
